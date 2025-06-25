341834dfe70e402736f350180cabbcb70da5a067
ThanhTumg <thanhtumg.2510@gmail.com>
2025-06-25 10:33:29 +0700

    Feature: Home Layout

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex border-t justify-between font-medium p-6">
+      <div className="fkex items-center gap-3">
+        <p>funroad, Inc.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, { useState } from "react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { MenuIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import NavBarSidebar from "./navbar-sidebar";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["700"],
+});
+
+interface NavbarItemProps {
+  href: string;
+  children: React.ReactNode;
+  isActive?: boolean;
+}
+
+const NavbarItem = ({ href, children, isActive }: NavbarItemProps) => {
+  return (
+    <Button
+      variant={"outline"}
+      className={cn(
+        "bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent px-3.5 text-lg",
+        isActive && "bg-black text-white hover:bg-black hover:text-white"
+      )}
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
+};
+
+const navbarItems = [
+  { href: "/", children: "Home" },
+  { href: "/about", children: "About" },
+  { href: "/features", children: "Features" },
+  { href: "/pricing", children: "Pricing" },
+  { href: "/contact", children: "Contact" },
+];
+
+const Navbar = () => {
+  const pathName = usePathname();
+
+  // States
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  // Template
+  return (
+    <nav className="h-20 flex border-b justify-between font-medium bg-white">
+      <Link href="/" className="pl-6 flex items-center">
+        <span className={cn("text-5xl font-semibold", poppins.className)}>
+          funroad
+        </span>
+      </Link>
+
+      {/* Sidebar */}
+      <NavBarSidebar
+        items={navbarItems}
+        open={isSidebarOpen}
+        onOpenChange={setIsSidebarOpen}
+      />
+
+      <div className="lg:flex items-center gap-4 hidden">
+        {navbarItems.map((item) => (
+          <NavbarItem
+            key={item.href}
+            href={item.href}
+            isActive={pathName === item.href}
+          >
+            {item.children}
+          </NavbarItem>
+        ))}
+      </div>
+
+      <div className="hidden lg:flex">
+        <Button
+          asChild
+          variant={"secondary"}
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
+        >
+          <Link href="/sign-in">Login</Link>
+        </Button>
+        <Button
+          asChild
+          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black hover:bg-pink-400 hover:text-black transition-colors text-lg"
+        >
+          <Link href="/sign-up">Start selling</Link>
+        </Button>
+      </div>
+
+      {/* Sidebar trigger mobile */}
+      <div className="flex lg:hidden justify-center items-center">
+        <Button
+          variant={"ghost"}
+          className="size-12 border-transparent bg-white"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
