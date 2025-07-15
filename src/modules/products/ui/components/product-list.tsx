9ea6b1bb4f8ce116b05e28c98838adca3acaa114
ThanhTumg <thanhtumg.2510@gmail.com>
2025-07-15 11:10:05 +0700

    Feature: Products

---
+++ src/modules/products/ui/components/product-list.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+interface Props {
+  category?: string;
+}
+
+const ProductList = ({ category }: Props) => {
+  // Server
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.products.getMany.queryOptions({ category })
+  );
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export default ProductList;
+
+export const ProductListSkeleton = () => {
+  return <div>Loading...</div>;
+};
