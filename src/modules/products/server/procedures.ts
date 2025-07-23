3012fd836a42c554a11f9a6bd3937b815d69976f
ThanhTumg <thanhtumg.2510@gmail.com>
2025-07-23 11:07:55 +0700

    Feature: Product page

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -119,4 +119,20 @@ export const productsRouter = createTRPCRouter({
         })),
       };
     }),
+
+  getOne: baseProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const product = await ctx.payload.findByID({
+        collection: "products",
+        id: input.id,
+        depth: 2, // Populate the "product.image", "product.tenant" and "product.tenant.image"
+      });
+
+      return {
+        ...product,
+        image: product.image as Media | null,
+        tenant: product.tenant as Tenant & { image: Media | null },
+      };
+    }),
 });
