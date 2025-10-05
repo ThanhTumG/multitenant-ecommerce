362cebd7b3f8ee89f8a33cba8e1f826f8bf144d6
ThanhTumg <thanhtumg.2510@gmail.com>
2025-10-05 09:49:34 +0700

    Chore

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -190,7 +190,6 @@ export interface Tenant {
  */
 export interface Media {
   id: string;
-  tenant?: (string | null) | Tenant;
   alt: string;
   updatedAt: string;
   createdAt: string;
@@ -428,7 +427,6 @@ export interface UsersSelect<T extends boolean = true> {
  * via the `definition` "media_select".
  */
 export interface MediaSelect<T extends boolean = true> {
-  tenant?: T;
   alt?: T;
   updatedAt?: T;
   createdAt?: T;
